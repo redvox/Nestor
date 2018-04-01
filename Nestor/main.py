a6061c6a075776fdaa34ac168496597a0f3fe84d
Jens Schaa <jens.schaa@posteo.de>
2018-04-01 19:06:37 +0200

    calendar prototype

--- Nestor/main.py
+++ Nestor/main.py
@@ -1,26 +1,33 @@
-import yaml
+import json
 import weather
-import display
+import my_calendar
 
 
-def read_config_file():
+# import display
+
+
+def read_config_file(name):
     try:
-        with open('./config/config.yaml') as config_file:
-            return yaml.load(config_file)
+        with open('./config/{}_config.json'.format(name)) as config_file:
+            return json.load(config_file)
     except KeyError as e:
         raise e
 
 
 def main():
-    config = read_config_file()
-    w = weather.get_weather(config['weather']['api_key'], config['weather']['zmw'])
+    # weather_config = read_config_file('weather')
+    # w = weather.get_weather(weather_config['api_key'], weather_config['zmw'])
     # print(w)
-    # w = {'temp': '4.8', 'feels_like': '5', 'icon': 'mostlycloudy', 'weather_icon': '\uf002',
-    #      'today': {'high': '3', 'low': '-2', 'icon': 'partlycloudy', 'weather_icon': '\uf002',
-    #                'conditions': 'Partly Cloudy'},
-    #      'tomorrow': {'high': '8', 'low': '4', 'icon': 'mostlycloudy', 'weather_icon': '\uf031',
-    #                   'conditions': 'Mostly Cloudy'}}
-    display.display(w)
+    w = {'temp': '4.8', 'feels_like': '5', 'icon': 'mostlycloudy', 'weather_icon': '\uf002',
+         'today': {'high': '3', 'low': '-2', 'icon': 'partlycloudy', 'weather_icon': '\uf002',
+                   'conditions': 'Partly Cloudy'},
+         'tomorrow': {'high': '8', 'low': '4', 'icon': 'mostlycloudy', 'weather_icon': '\uf031',
+                      'conditions': 'Mostly Cloudy'}}
+
+    calendar_config = read_config_file('calendar')
+    my_calendar.calendar(calendar_config)
+
+    # display.display(w)
 
 
 if __name__ == '__main__':
--- Nestor/my_calendar.py
+++ Nestor/my_calendar.py
@@ -1,14 +1,27 @@
 # Sample code was taken from https://developers.google.com/calendar/quickstart/python
 
 import httplib2
-import os
+import json
 
 from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
+from pprint import pprint
+from dateutil import parser
 
 import datetime
+
+
+def get_events(service, calendar_id):
+    now = datetime.datetime.utcnow().isoformat() + 'Z'
+    events_result = service.events().list(
+        calendarId=calendar_id,
+        timeMin=now,
+        maxResults=10,
+        singleEvents=True,
+        orderBy='startTime').execute()
+    return events_result.get('items', [])
 
 
 def get_credentials():
@@ -35,7 +48,30 @@ def get_credentials():
     return credentials
 
 
-def main():
+def get_weekday(day):
+    if day == 0: return "Montag"
+    if day == 1: return "Dienstag"
+    if day == 2: return "Mittwoch"
+    if day == 3: return "Donnerstag"
+    if day == 4: return "Freitag"
+    if day == 5: return "Samstag"
+    if day == 6: return "Sonntag"
+
+
+def uniform_event(event):
+    start = parser.parse(event['start'].get('dateTime', event['start'].get('date')))
+    end = parser.parse(event['start'].get('dateTime', event['start'].get('date')))
+    # print(dt.weekday())
+    date_format = "%d-%m-%Y %H:%M:%S"
+    return {
+        'summary': event['summary'],
+        'start': start.strftime(date_format),
+        'end': end.strftime(date_format),
+        'weekday': get_weekday(start.weekday())
+    }
+
+
+def calendar(calendar_config):
     """Shows basic usage of the Google Calendar API.
 
     Creates a Google Calendar API service object and outputs a list of the next
@@ -45,18 +81,14 @@ def main():
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
 
-    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    eventsResult = service.events().list(
-        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
-        orderBy='startTime').execute()
-    events = eventsResult.get('items', [])
+    full_calender = dict()
 
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+    for name, calendar_id in calendar_config.items():
+        full_calender[name] = list()
+        events = get_events(service, calendar_id)
+        for event in events:
+            full_calender[name].append(uniform_event(event))
+    pprint(full_calender)
 
 
 if __name__ == '__main__':
