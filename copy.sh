7e3e8087fd330686d7105cfe91b4963c56cdfcb0
Jens Schaa <jens.schaa@posteo.de>
2018-04-01 17:55:15 +0200

    run-remote can now copy and setup

--- copy.sh
+++ copy.sh
@@ -1,36 +1,10 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
+TARGET_IP=${1}
+MODE_ALL=${2}
 
-MODE_SETUP="false"
-
-usage() {   echo -e "${CB}Usage:${CC}"
-            echo    " copy.sh [-s][-h]"
-            echo
-            echo    "Copies your py files from Nestor to remote."
-            echo
-            echo -e "${CB}Options:${CC}"
-            echo    "   -s      copy all files"
-            exit 1; }
-
-while getopts "sh-:" opt; do
-  case ${opt} in
-    s)
-      MODE_SETUP="true"
-      ;;
-    h)
-      usage
-      ;;
-    *)
-      echo -e "${CR}Invalid option:${CC} -$OPTARG"
-      usage
-      ;;
-  esac
-done
-
-TARGET_IP=192.168.178.42
-
-if [ "${MODE_SETUP}" == "true" ]; then
+if [ "${MODE_ALL}" == "true" ]; then
     scp -pr ./Nestor pi@${TARGET_IP}:/home/pi/
     scp -pr ./config pi@${TARGET_IP}:/home/pi/
     scp -pr ./requirements.txt pi@${TARGET_IP}:/home/pi/
--- run-remote.sh
+++ run-remote.sh
@@ -4,5 +4,45 @@ set -e
 
 TARGET_IP=192.168.178.42
 
-./copy.sh
+MODE_ALL="false"
+MODE_SETUP="false"
+
+usage() {   echo -e "${CB}Usage:${CC}"
+            echo    " run-remote.sh [-s][-a][-h]"
+            echo
+            echo    "Copies your py files from Nestor to remote and execute them"
+            echo
+            echo -e "${CB}Options:${CC}"
+            echo    "   -a      copy all files"
+            echo    "   -s      run setup"
+            exit 1; }
+
+while getopts "ash-:" opt; do
+  case ${opt} in
+    a)
+      MODE_ALL="true"
+      ;;
+    s)
+      MODE_SETUP="true"
+      ;;
+    h)
+      usage
+      ;;
+    *)
+      echo -e "${CR}Invalid option:${CC} -$OPTARG"
+      usage
+      ;;
+  esac
+done
+
+if [ "${MODE_ALL}" == "true" ]; then
+    ./copy.sh ${TARGET_IP} true
+else
+    ./copy.sh ${TARGET_IP}
+fi
+
+if [ "${MODE_SETUP}" == "true" ]; then
+    ssh pi@${TARGET_IP} ./setup.sh
+fi
+
 ssh pi@${TARGET_IP} ./run.sh
