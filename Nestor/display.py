fc226ff8fe9fe264bbfeffcfd86abd0bb527c470
Jens Schaa <jens.schaa@posteo.de>
2018-04-27 19:18:26 +0200

    invert image because the display is greyed out. Also add test image

--- Nestor/display.py
+++ Nestor/display.py
@@ -25,14 +25,15 @@
 # THE SOFTWARE.
 ##
 from os import environ
+from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
 
 if not environ.get("DRY_RUN"):
     from epd import epd7in5
 
-from PIL import Image, ImageDraw, ImageFont
-
 EPD_WIDTH = 640
 EPD_HEIGHT = 384
+COLOR = 255  # white (255) or black (0)
 
 
 def truncate(string, max_length):
@@ -48,7 +49,7 @@ def display_departures(draw, departures):
     departures_string = []
     for departure in departures:
         departures_string.append(departure['time'] + ' ' + departure['delay'] + ' <' + departure['line'] + '>')
-    draw.text((x, y), '   •   '.join(departures_string), font=font_bold, fill=0)
+    draw.text((x, y), '   •   '.join(departures_string), font=font_bold, fill=COLOR)
 
 
 def display_calendar(draw, calendar):
@@ -66,7 +67,7 @@ def display_calendar(draw, calendar):
             weekday = event['weekday']
             y = 50
             x += 132
-            draw.text((x, y), event['weekday'], font=font_bold, fill=0)
+            draw.text((x, y), event['weekday'], font=font_bold, fill=COLOR)
 
         y += text_size_px
         if event['start_hour'] != '00:00':
@@ -76,10 +77,10 @@ def display_calendar(draw, calendar):
 
         draw.text((x, y), event_test.format(**event),
                   font=font_bold,
-                  fill=0)
+                  fill=COLOR)
 
         y += text_size_px
-        draw.text((x, y), truncate(event['summary'], 18), font=font_light, fill=0)
+        draw.text((x, y), truncate(event['summary'], 18), font=font_light, fill=COLOR)
 
 
 def display_weather(draw, weather):
@@ -91,38 +92,47 @@ def display_weather(draw, weather):
     weather_tomorrow = weather['tomorrow']
 
     # WEATHER NOW
-    draw.text((0, 0), weather_now['weather_icon'], font=font_weather_icons, fill=0)
+    draw.text((0, 0), weather_now['weather_icon'], font=font_weather_icons, fill=COLOR)
     today_string = weather_now['temp'] + " (" + weather_now['feels_like'] + ")"
-    draw.text((40, 0), today_string, font=font_black, fill=0)
+    draw.text((40, 0), today_string, font=font_black, fill=COLOR)
 
     # WEATHER TODAY
-    draw.text((180, 0), weather_today['weather_icon'], font=font_weather_icons, fill=0)
+    draw.text((180, 0), weather_today['weather_icon'], font=font_weather_icons, fill=COLOR)
     today_string = weather_today['high'] + " / " + weather_today['low']
-    draw.text((220, 0), today_string, font=font_black, fill=0)
+    draw.text((220, 0), today_string, font=font_black, fill=COLOR)
 
     # WEATHER TOMORROW
-    draw.text((320, 0), weather_tomorrow['weather_icon'], font=font_weather_icons, fill=0)
+    draw.text((320, 0), weather_tomorrow['weather_icon'], font=font_weather_icons, fill=COLOR)
     tomorrow_string = weather_tomorrow['high'] + " / " + weather_tomorrow['low']
-    draw.text((360, 0), tomorrow_string, font=font_black, fill=0)
+    draw.text((360, 0), tomorrow_string, font=font_black, fill=COLOR)
 
 
-def render(now, weather, calendar, departures):
-    # clear
+def black_screen(draw):
+    draw.rectangle((0, 0, EPD_WIDTH, EPD_HEIGHT), fill=0)
+
+
+def display_update_timestamp(draw):
+    font_bold = ImageFont.truetype('Nestor/fonts/Roboto-Bold.ttf', 14)
+
+    date_format = "%d-%m-%Y %H:%M"
+    now = datetime.now().strftime(date_format)
+
+    draw.text((525, 0), 'Last Refesh', font=font_bold, fill=COLOR)
+    draw.text((525, 20), now, font=font_bold, fill=COLOR)
+
+
+def render(weather, calendar, departures, dry_run):
     image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 1)
     draw = ImageDraw.Draw(image)
 
-    # fonts
-    font_bold = ImageFont.truetype('Nestor/fonts/Roboto-Bold.ttf', 14)
-
-    # UPDATE DATE
-    draw.text((525, 0), 'Last Refesh', font=font_bold, fill=0)
-    draw.text((525, 20), now, font=font_bold, fill=0)
-
-    display_weather(draw, weather)
+    if not dry_run:
+        black_screen(draw)
+    display_update_timestamp(draw)
+    # display_weather(draw, weather)
     display_calendar(draw, calendar)
     display_departures(draw, departures)
 
-    if not environ.get("DRY_RUN"):
+    if not dry_run:
         push_to_display(image)
     else:
         image.save("display.png", "PNG")
@@ -131,4 +141,22 @@ def render(now, weather, calendar, departures):
 def push_to_display(image):
     epd = epd7in5.EPD()
     epd.init()
+    epd.display_frame(epd.get_frame_buffer(image))
+
+
+def render_test_image():
+    from epd import epd7in5
+    epd = epd7in5.EPD()
+    epd.init()
+    image = Image.open('Nestor/images/monocolor.bmp')
+    epd.display_frame(epd.get_frame_buffer(image))
+
+
+def render_black_screen():
+    from epd import epd7in5
+    epd = epd7in5.EPD()
+    epd.init()
+    image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 1)
+    draw = ImageDraw.Draw(image)
+    black_screen(draw)
     epd.display_frame(epd.get_frame_buffer(image))
