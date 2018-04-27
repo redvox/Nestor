39435686bc75a86aae52b5051834295f732b1dc3
Jens Schaa <jens.schaa@posteo.de>
2018-04-27 19:16:38 +0200

    fix run script and add dry run parameter

--- run.sh
+++ run.sh
@@ -6,9 +6,14 @@ if [ ! -d ${DIR}/venv ]; then
     setup.sh
 fi
 
+set +e
 MODE_RASPBERRY=$(uname -a | grep 'raspberry')
-if [ "${MODE_RASPBERRY}" != "" ]; then
+set -e
+if [ "${MODE_RASPBERRY}" == "" ]; then
+    echo "DRY RUN"
     export DRY_RUN=true
+    ${DIR}/venv/bin/python3 ${DIR}/Nestor/main.py --dry
+else
+    echo "RUN"
+    ${DIR}/venv/bin/python3 ${DIR}/Nestor/main.py
 fi
-
-${DIR}/venv/bin/python3 ${DIR}/Nestor/main.py
